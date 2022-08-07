be381a45eace69093587b472ecc33cb7cde1e164
Margarida <margaridammcm@gmail.com>
2022-08-07 09:43:30 -0300

    Create articles

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const articles = [{
+    title: 'Test Article',
+    createdAt: new Date(),
+    description: 'Test Description'
+  },
+  {
+    title: 'Test Article 2',
+    createdAt: new Date(),
+    description: 'Test Description 2'
+  }
+]
+  res.render('index', {articles: articles})
 })
 
 app.listen(5000)
