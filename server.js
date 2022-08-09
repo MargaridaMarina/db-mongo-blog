a40419651664f8acc87f24446815c080584f5db0
Margarida <margaridammcm@gmail.com>
2022-08-08 22:51:50 -0300

    create routes

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
+const cors = require('cors')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
+const Category = require('./models/category')
 const app = express()
 
 mongoose.connect('mongodb://127.0.0.1/blog')
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({createdAt:'desc'})
+  console.log({articles})
   res.render('articles/index', {articles: articles})
+  // res.json(articles)
+})
+
+app.get('/categorias', async (req, res) => {
+  const articles = await Category.find().sort({createdAt:'desc'})
+  console.log("category")
+  // res.render('articles/index', {articles: articles})
+  res.json(articles)
+})
+
+app.get('/posts', async (req, res) => {
+  const articles = await Article.find().sort({createdAt:'desc'})
+  console.log("post")
+  // res.render('articles/index', {articles: articles})
+  res.json(articles)
 })
 
 app.use('/articles', articleRouter)
