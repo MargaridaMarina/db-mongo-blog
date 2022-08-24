fc069d7dd164d99a3a1c1dd2accfc2e0cff169f9
Margarida <margaridammcm@gmail.com>
2022-08-23 23:14:07 -0300

    Edit route

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -4,10 +4,10 @@ import CategoryController from "../controllers/categoriesControllers.js"
 const router = express.Router()
 
 router
-  .get("/categories", CategoryController.listCategories)
-  .post("/categories", CategoryController.registerCategory)
-  .put("/categories/:id", CategoryController.updateCategory)
-  .get("/categories/:id", CategoryController.listCategoriesById)
-  .delete("/categories/:id", CategoryController.deleteCategory)
+  .get("/categorias", CategoryController.listCategories)
+  .post("/categorias", CategoryController.registerCategory)
+  .put("/categorias/:id", CategoryController.updateCategory)
+  .get("/categorias/:id", CategoryController.listCategoriesById)
+  .delete("/categorias/:id", CategoryController.deleteCategory)
 
 export default router
