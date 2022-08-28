02540b70d446e6c7ef663570305a90f51d577514
Margarida <margaridammcm@gmail.com>
2022-08-28 20:21:28 -0300

    lint

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -3,24 +3,28 @@ import posts from '../models/post.js'
 
 class PostController {
   static listPosts = (req, res) => {
-    posts.find()
+    posts
+      .find()
       .populate('category')
       .exec((err, posts) => {
-      res.status(200).json(posts)
-    })
+        res.status(200).json(posts)
+      })
   }
 
   static listPostsById = (req, res) => {
     const id = req.params.id
-    posts.findById(id)
+    posts
+      .findById(id)
       .populate('category', 'name')
       .exec((err, posts) => {
-      if(err) {
-        res.status(400).send({message: `${err.message} - Id do post não localizado.`})
-      } else {
-        res.status(200).send(posts)
-      }
-    })
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Id do post não localizado.` })
+        } else {
+          res.status(200).send(posts)
+        }
+      })
   }
 
   static registerPost = (req, res) => {
@@ -30,7 +34,7 @@ class PostController {
     let cat = new categories({
       name: category
     })
-    
+
     cat.save().then(data => {
       console.log(data)
 
@@ -53,22 +57,22 @@ class PostController {
 
   static updatePost = (req, res) => {
     const id = req.params.id
-    posts.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-      if(!err) {
-        res.status(200).send({message: 'Post atualizado com sucesso!'})
+    posts.findByIdAndUpdate(id, { $set: req.body }, err => {
+      if (!err) {
+        res.status(200).send({ message: 'Post atualizado com sucesso!' })
       } else {
-        res.status(500).send({message: err.message})
+        res.status(500).send({ message: err.message })
       }
     })
   }
 
   static deletePost = (req, res) => {
     const id = req.params.id
-    posts.findByIdAndDelete(id, (err) => {
-      if(!err) {
-        res.status(200).send({message: 'Post removido com sucesso!'})
+    posts.findByIdAndDelete(id, err => {
+      if (!err) {
+        res.status(200).send({ message: 'Post removido com sucesso!' })
       } else {
-        res.status(500).send({message: err.message})
+        res.status(500).send({ message: err.message })
       }
     })
   }
